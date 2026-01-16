2286b10b390916e7e4f8ab08b395dad83e8c4832
Victor Bishel <matodorrrrr@gmail.com>
2026-01-16 14:40:09 +0700

    [NF] auth service

--- auth-service/internal/domain/service/auth/auth.go
+++ auth-service/internal/domain/service/auth/auth.go
@@ -4,8 +4,11 @@ import (
 	"auth-service/config"
 	"auth-service/internal/domain/entity"
 	"auth-service/internal/lib/jwt"
+	"auth-service/internal/lib/logger/sl"
 	"auth-service/pkg/apperrors"
 	"context"
+	"errors"
+	"log/slog"
 )
 
 type UserRepository interface {
@@ -15,11 +18,12 @@ type UserRepository interface {
 }
 
 type authService struct {
+	log            *slog.Logger
 	cfg            *config.Config
 	userRepository UserRepository
 }
 
-func NewAuthService(userRepository UserRepository, cfg *config.Config) *authService {
+func New(userRepository UserRepository, cfg *config.Config) *authService {
 	return &authService{
 		userRepository: userRepository,
 		cfg:            cfg,
@@ -27,6 +31,15 @@ func NewAuthService(userRepository UserRepository, cfg *config.Config) *authServ
 }
 
 func (s *authService) Register(ctx context.Context, registerDTO RegisterDTO) (entity.UserID, error) {
+	const op = "AuthService.Register"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.String("email", registerDTO.email),
+	)
+
+	log.Info("attemtping to register user")
+
 	u := entity.User{
 		Email:    registerDTO.email,
 		Password: registerDTO.password,
@@ -35,15 +48,33 @@ func (s *authService) Register(ctx context.Context, registerDTO RegisterDTO) (en
 
 	uid, err := s.userRepository.Create(ctx, u)
 	if err != nil {
+		log.Error("failed to create user", sl.Err(err))
 		return 0, err
 	}
+
+	log.Info("registered new user", "UserID", uid)
 
 	return uid, nil
 }
 
 func (s *authService) Login(ctx context.Context, loginDTO LoginDTO) (string, error) {
+	const op = "AuthService.Login"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.String("email", loginDTO.email),
+	)
+
+	log.Info("attempting to login user")
+
 	u, err := s.userRepository.FindByEmail(ctx, loginDTO.email)
 	if err != nil {
+		if errors.Is(err, apperrors.ErrUserNotFound) {
+			log.Warn("user not found", sl.Err(err))
+			return "", apperrors.ErrUserIncorrectEmailOrPassword
+		}
+
+		log.Error("failed to get user", sl.Err(err))
 		return "", err
 	}
 
@@ -55,6 +86,8 @@ func (s *authService) Login(ctx context.Context, loginDTO LoginDTO) (string, err
 	if err != nil {
 		return "", err
 	}
+
+	log.Info("user logged in")
 
 	return token, nil
 }
